67ec4a4be3d2319365c0db70a1b63e9029b779bf
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2021-09-06 19:02:29 +0530

    added the code

---
+++ src/main/java/com/vishwa/microservice1/LoadBalancerConfigurations.java
@@ -0,0 +1,22 @@
+package com.vishwa.microservice1;
+
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class LoadBalancerConfigurations {
+
+  @Bean
+  public ServiceInstanceListSupplier
+  discoveryClientServiceInstanceListSupplier(
+      ConfigurableApplicationContext context) {
+    System.out.println("Configuring Load balancer to prefer same instance");
+    return ServiceInstanceListSupplier.builder()
+        .withBlockingDiscoveryClient()
+        .withSameInstancePreference()
+        .build(context);
+  }
+}
--- src/main/java/com/vishwa/microservice1/Microservice2Client.java
+++ src/main/java/com/vishwa/microservice1/Microservice2Client.java
@@ -1,10 +1,13 @@
 package com.vishwa.microservice1;
 
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 //@FeignClient(name="MC2Client", url="http://localhost:7072/")
+@LoadBalancerClient(name = "MICROSERVICE2",
+    configuration=LoadBalancerConfigurations.class)
 @FeignClient(name="MICROSERVICE2")
 public interface Microservice2Client {
 
